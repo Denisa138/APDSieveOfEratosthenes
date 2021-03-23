7fce382ae08aeba5c57791795a743c3d967089d9
Denisa138 <stroie_denisa99@yahoo.com>
2021-03-23 21:14:08 +0200

    Update tests

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Results results = new Results(11, "input", "output", 1000, 50000, 1000);
+        Results results = new Results(10, "input", "output", 9500, 12000, 11000);
 
         long startTime = System.currentTimeMillis();
         results.generateResults();
